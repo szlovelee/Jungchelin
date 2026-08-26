2891fd1044d01bc2517f1287d439111e87c8e4b4
노도현 <dhrho1208@gmail.com>
2026-08-26 14:49:04 +0900

    update 마이페이지 구현

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,3 +7,5 @@ bp = Blueprint("routes", __name__)
 from . import auth_routes
 from . import page_route
 from . import resto_routes
+from . import review_routes
+from . import mypage_routes
--- app/routes/page_route.py
+++ app/routes/page_route.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, request
 
 from . import bp
 from app.services import resto_service
+from app.services import review_service
+from app.services import user_service
 from app.utils import jwt_required
 from app.utils.jwt_utils import get_user_id_from_token
 
@@ -13,13 +15,21 @@ def index_page():
 
 @bp.route("/preview")
 def preview_home():
-    restaurants = resto_service.load_resto_list()
+    sort_key = request.args.get(
+        "sort",
+        "star"
+    )
+
+    restaurants = resto_service.load_resto_list(
+        sort_key
+    )
 
     return render_template(
         "home.html",
         restaurants=restaurants,
         selected_restaurant=None,
-        reviews=[]
+        reviews=[],
+        current_sort=sort_key
     )
 
 
@@ -27,21 +37,47 @@ def preview_home():
 @jwt_required
 def home_page():
     user_id = get_user_id_from_token()
-    sort_key = request.args.get("sort")
 
-    restaurants = resto_service.load_resto_list(sort_key)
+    sort_key = request.args.get(
+        "sort",
+        "star"
+    )
 
-    restaurant_id = request.args.get("restaurant_id")
+    restaurants = resto_service.load_resto_list(
+        sort_key
+    )
+
+    restaurant_id = request.args.get(
+        "restaurant_id"
+    )
+
     selected_restaurant = None
     reviews = []
 
     if restaurant_id:
-        selected_restaurant = resto_service.get_resto_detail(restaurant_id)
+        selected_restaurant = (
+            resto_service.get_resto_detail(
+                restaurant_id
+            )
+        )
+
+        if selected_restaurant:
+            reviews = review_service.get_review_list(
+                restaurant_id
+            )
+
+            for review in reviews:
+                review["nickname"] = (
+                    user_service.get_user_name(
+                        str(review["user"])
+                    )
+                )
 
     return render_template(
         "home.html",
         user_id=user_id,
         restaurants=restaurants,
         selected_restaurant=selected_restaurant,
-        reviews=reviews
+        reviews=reviews,
+        current_sort=sort_key
     )
